a4fdbd670c2d5b11fbb07b44c008b40ec19408fe
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-10-22 00:32:32 +0530

    done with qr-code project  6

---
+++ reactpro6/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import {QRCode} from 'react-qr-code'
+const App =()=>{
+const [input , setInput] = useState('')
+const [genrate , setGEnrate] = useState('')
+
+const handleButton = ()=>{
+  setGEnrate(input)
+  setInput('')
+}
+
+  return (
+    <div className="" style={{
+      display:'flex',
+      flexDirection:'column',
+      width:'50%',
+      gap:10,
+    }}>
+      <input type="text" placeholder="Enter the qr code you wanted" name='qr-code' value={input} 
+      onChange={(e)=>setInput(e.target.value)}
+      />
+      <button onClick={handleButton}>Submit</button>
+
+      <QRCode value={genrate} size={400} bgColor="#fff" id="qr-codee" />
+
+
+
+
+    </div>
+  )
+}
+
+
+export default App
